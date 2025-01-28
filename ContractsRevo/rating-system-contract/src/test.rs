c248476c0eff0d5692418574dd154806afdab305
Fabricio Robles <fabrobles92@gmail.com>
2025-01-27 22:42:53 -0600

    feat: create test_rate_seller that test the contract function 'rate_seller'

--- ContractsRevo/rating-system-contract/src/test.rs
+++ ContractsRevo/rating-system-contract/src/test.rs
@@ -1,7 +1,8 @@
 #![cfg(test)]
 
+use crate::rating::Rating;
 use super::*;
-use soroban_sdk::{testutils::Address as TestAddress, Env};
+use soroban_sdk::{testutils::Address as TestAddress, Env, Vec};
 
 #[test]
 fn test_rate_seller() {
@@ -10,17 +11,31 @@ fn test_rate_seller() {
     let client = RatingSytemContractClient::new(&env, &contract_id);
     let seller_address = <Address>::generate(&env);
     let buyer_address = <Address>::generate(&env);  
-    let rating = 3u32;
-    let weight = 1u32;  
-    
+    let rating = 3u32;  
+    let weight = 2u32;  
+    let weighted_key = DataKey::WeightedRating(seller_address.clone());
+    let rating_key = DataKey::Rating(seller_address.clone());
 
     client.rate_seller(&seller_address, &buyer_address, &rating, &weight, &None);
-
+    
     env.as_contract(&contract_id, || {
-        let (total_weighted_rating,total_weight): (u32, u32) = env.storage().instance().get(&seller_address).unwrap();
-        assert_eq!(total_weighted_rating, rating);
+        let (total_weighted_rating,total_weight): (u32, u32) = env.storage().instance().get(&weighted_key).unwrap();
+        let ratings: Vec<Rating> = env.storage().instance().get(&rating_key).unwrap();
+
+        assert_eq!(ratings.len(), 1);
+        assert_eq!(total_weighted_rating, rating*weight);
         assert_eq!(total_weight, weight);
     });
 
+    client.rate_seller(&seller_address, &buyer_address, &rating, &weight, &None);
+    client.rate_seller(&seller_address, &buyer_address, &rating, &weight, &None);
 
+    env.as_contract(&contract_id, || {
+        let (total_weighted_rating,total_weight): (u32, u32) = env.storage().instance().get(&weighted_key).unwrap();
+        let ratings: Vec<Rating> = env.storage().instance().get(&rating_key).unwrap();
+
+        assert_eq!(ratings.len(), 3);
+        assert_eq!(total_weighted_rating, (rating*weight)*3);
+        assert_eq!(total_weight, weight * 3);
+    });
 }
